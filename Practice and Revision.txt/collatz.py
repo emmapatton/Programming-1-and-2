5cd884fa2ba4e3eb2d0626b5719fec0253a83982
emmapatton <G00364733@gmit.ie>
2018-03-29 17:33:56 +0100

    adding revision folder

---
+++ Practice and Revision.txt/collatz.py
@@ -0,0 +1,22 @@
+#Emma Patton 2018-02-11
+#The Collatz Conjecture Program: https://en.wikipedia.org/wiki/Collatz_conjecture
+
+nString = input("Enter a number: ")
+n = int(nString)
+ 
+count = 0
+print("starting value:", n)
+
+while n != 1:
+
+    if(n % 2 == 0):
+        n = int(n / 2)
+    else:
+        n = int(3 * n + 1)
+
+    print(n)
+    count = count + 1
+
+
+print('Program finished in', count, 'steps')
+    
